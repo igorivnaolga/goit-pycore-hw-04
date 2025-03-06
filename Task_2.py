1a7dc8d20df0ad02ac177954c77166e9d04ee30e
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2025-03-06 07:43:16 +0200

    task 2 done

--- Task_2.py
+++ Task_2.py
@@ -19,35 +19,29 @@
 # Функція має повертати список словників, де кожен словник містить інформацію про одного кота.
 
 
-# Рекомендації для виконання:
+def get_cats_info(path):
+    try:
+        cats = []
 
-# Використовуйте with для безпечного читання файлу.
-# Пам'ятайте про встановлення кодування при відкриті файлів
-# Для кожного рядка в файлі використовуйте split(',') для отримання ідентифікатора, імені та віку кота.
-# Утворіть словник з ключами "id", "name", "age" для кожного кота та додайте його до списку, який буде повернуто.
-# Опрацьовуйте можливі винятки, пов'язані з читанням файлу.
+        with open(path, encoding = "utf-8") as file:
+            for line in file:
+                cleaned_line = line.strip()
+                if cleaned_line:
+                    cat_id, name, age = line.split(',')
+                    cat = {"id": cat_id, "name": name, "age": int(age)}
+                    cats.append(cat)
+
+        return cats
+    
+    except FileNotFoundError:
+        print(f"File '{path}' not found.")
+        return []
+    except ValueError:
+        print("Invalid data format in file.")
+        return []
+    
+cats_info = get_cats_info("cats.txt")
+print(cats_info)
 
 
-# Критерії оцінювання:
-
-# Функція має точно обробляти дані та повертати правильний список словників.
-# Повинна бути належна обробка винятків і помилок.
-# Код має бути чистим, добре структурованим і зрозумілим.
-
-
-# Приклад використання функції:
-
-# cats_info = get_cats_info("path/to/cats_file.txt")
-# print(cats_info)
-
-
-
-# Очікуваний результат:
-
-# [
-#     {"id": "60b90c1c13067a15887e1ae1", "name": "Tayson", "age": "3"},
-#     {"id": "60b90c2413067a15887e1ae2", "name": "Vika", "age": "1"},
-#     {"id": "60b90c2e13067a15887e1ae3", "name": "Barsik", "age": "2"},
-#     {"id": "60b90c3b13067a15887e1ae4", "name": "Simon", "age": "12"},
-#     {"id": "60b90c4613067a15887e1ae5", "name": "Tessi", "age": "5"},
-# ]
+        
